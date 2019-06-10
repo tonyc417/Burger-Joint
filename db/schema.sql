ad94764837fdaae37fd56f4fb418522447b73970
Tony Cruz <tonycruz@Tonys-MBP.frontierlocal.net>
2019-06-09 23:21:52 -0500

    created seeds and schema

--- db/schema.sql
+++ db/schema.sql
@@ -2,9 +2,9 @@ CREATE DATABASE burgers_db;
 
 USE burgers_db;
 
-CREATE TABLE burgers_db(
-    id INTEGER NOT NULL AUTO_INCREMENT,
+CREATE TABLE burgers(
+    id INTEGER(30) AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(50) NOT NULL,
     devoured BOOLEAN NOT NULL DEFAULT 0,
     PRIMARY KEY(id)
-)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers(burger_name, devoured)
+VALUES ('Bacon Burger', false);
+
+INSERT INTO burgers(burger_name, devoured)
+VALUES ('Mushroom Swiss', false);
+
+INSERT INTO burgers(burger_name, devoured)
+VALUES ('Santa Fe Burger', false);
