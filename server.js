666d202b351d59f6940bfe6879c5521319bcfd5e
Tony Cruz <tonycruz@Tonys-MBP.frontierlocal.net>
2019-06-09 23:05:44 -0500

    added mysql code

--- server.js
+++ server.js
@@ -1,2 +1,10 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
+
 const mysql = require('mysql');
+
+var PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.listen(PORT, console.log("Server Started buddy"));
